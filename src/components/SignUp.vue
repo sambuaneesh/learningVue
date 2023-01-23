1376e931be25e6371182240f2510ca34bde4734e
Aneesh Sambu <75294086+stealthspectre@users.noreply.github.com>
2023-01-23 21:51:07 +0530

    SignUp page template

---
+++ src/components/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <img class="logo" src="../assets/logo.png" />
+  <!-- Header -->
+  <h1>Sign Up</h1>
+  <!-- Input Fields -->
+  <div class="register">
+    <input type="text" placeholder="Enter Name" />
+    <input type="text" placeholder="Enter Email" />
+    <input type="password" placeholder="Enter Password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+.logo {
+  width: 150px;
+}
+
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid #110c24;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  background: #151415;
+  color: white;
+  cursor: pointer;
+}
+</style>
